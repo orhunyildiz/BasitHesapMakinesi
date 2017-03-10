e97e14a087025674da71ace93e4167512a9cb9d3
Orhun YILDIZ <orhunyyildiz@gmail.com>
2017-03-10 22:25:15 +0300

    Update 3

--- GitHubHesapMakinesi/GitHubHesapMakinesi/Program.cs
+++ GitHubHesapMakinesi/GitHubHesapMakinesi/Program.cs
@@ -12,35 +12,40 @@ namespace GitHubHesapMakinesi
         {
             double sayi1, sayi2, sayi3;
             Console.WriteLine("*****Orhun'un Console Hesap Makinesine Hoşgeldiniz!*****\n");
-            Console.Write("Birinci sayıyı giriniz: ");
-            sayi1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("İkinci sayıyı giriniz: ");
-            sayi2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
-            Console.WriteLine("- Yapmak İstediğiniz İşlemi Seçiniz -\n1 - Toplama\n2 - Çıkarma\n3 - Çarpma\n4 - Bölme\n");
-            Console.Write("Yapmak İstediğiniz İşlemin Başındaki Katsayıyı Giriniz: ");
-            sayi3 = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine();
-            if (sayi3 == 1)
+            do
             {
-                Console.Write("Sonuç: " + (sayi1 + sayi2));
+                Console.Write("Birinci sayıyı giriniz: ");
+                sayi1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("İkinci sayıyı giriniz: ");
+                sayi2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine("- Yapmak İstediğiniz İşlemi Seçiniz -\n1 - Toplama\n2 - Çıkarma\n3 - Çarpma\n4 - Bölme\n");
+                Console.Write("Yapmak İstediğiniz İşlemin Başındaki Katsayıyı Giriniz: ");
+                sayi3 = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine();
+                if (sayi3 == 1)
+                {
+                    Console.Write("Sonuç: " + (sayi1 + sayi2) + "\n\n");
+                }
+                else if (sayi3 == 2)
+                {
+                    Console.Write("Sonuç: " + (sayi1 - sayi2) + "\n\n");
+                }
+                else if (sayi3 == 3)
+                {
+                    Console.Write("Sonuç: " + (sayi1 * sayi2) + "\n\n");
+                }
+                else if (sayi3 == 4)
+                {
+                    Console.Write("Sonuç: " + (sayi1 / sayi2) + "\n\n");
+                }
+                else
+                {
+                    Console.Write("Lütfen Geçerli Bir İfade Giriniz!");
+                }
             }
-            else if (sayi3 == 2)
-            {
-                Console.Write("Sonuç: " + (sayi1 - sayi2));
-            }
-            else if (sayi3 == 3)
-            {
-                Console.Write("Sonuç: " + (sayi1 * sayi2));
-            }
-            else if (sayi3 == 4)
-            {
-                Console.Write("Sonuç: " + (sayi1 / sayi2));
-            }
-            else
-            {
-                Console.Write("Lütfen Geçerli Bir İfade Giriniz!");
-            }
+            while (sayi3 != 56);
+
             Console.ReadKey();
         }
     }
